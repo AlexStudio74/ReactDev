c4a1e57325e15ac6d61be3a6df35e899e7e43e1b
AlexStudio74 <mrmahonin@yandex.ru>
2023-06-02 18:18:54 +0500

    Практика часть1

--- js/script.js
+++ js/script.js
@@ -2,5 +2,22 @@
 
 //alert('жопа');
 
-const result = confirm("Ты не ЛОХ!?");
-console.log(result);
+const NumberOfFilms = +prompt("Сколько фильмов вы посмотрели?", " ");
+
+const personalMoveDB = {
+    count: NumberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('Оцените его из 10', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('Оцените его из 10', '');
+
+personalMoveDB.movies[a] = b;
+personalMoveDB.movies[c] = d;
+
+console.log(personalMoveDB);
