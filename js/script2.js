02e505eb98a72f081294349d6931a73d69df875b
AlexStudio74 <mrmahonin@yandex.ru>
2023-06-04 22:55:24 +0500

    18. Массивы и псевдомассивы

--- js/script2.js
+++ js/script2.js
@@ -1,35 +1,14 @@
 "use strict";
 
-const options = {
-    name: "test",
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    }
-};
-
-
-const {border, bg} = options.colors;
-
-//delete options.name;
+//const arr = [1,2,3,6,8];
+////arr.pop();
+//arr.push(10);
+//console.log(arr);
 //
-//let counter = 0;
-//
-//
-//for(let key in options){
-//    if(typeof(options[key]) === 'object'){
-//        for(let i in options[key]){
-//            console.log(`Свойство ${key}/${i} имеет значение ${options[key][i]}`);
-//            counter++;
-//        }
-//    }else{
-//        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//        counter++;
-//    }
-//    
-//};
-//
-//console.log(`Переменная ${"options"} имеет в себе ${counter} значений`);
+//arr.forEach(function(item, i, arr){
+//    console.log(`${i}: ${item} в массиве ${arr}`);
+//});
 
+const str = prompt("", "");
+const products = str.split(",");
+console.log(products);
